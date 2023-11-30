ceaa711903052c8a036f3947de84a35c8458eb46
Zoltan_Mark_Antal <zotyaa95@gmail.com>
2023-11-30 11:17:33 +0100

    added bootstrap components

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,37 +1,64 @@
-console.log('loaded')
 const rootElement = document.querySelector("#root")
 
 const fetchUrl = async (url) => {
   const response = await fetch(url)
-  console.log(response)
   return response.json()
 }
 
-const beerComponent = (beer) => `
-  <div>
-    <h2>${beer.id}</h2>
-    <h3>${beer.name}</h3>
-    <h4>${beer.abv}</h4>
+const skeleton = () => `
+  <header></header>
+  <main></main>
+`
+
+const beerComponent = ({id, name, abv}) => `
+  <div class="card text-bg-success mb-3" style="max-width: 18rem;">
+    <div class="card-header">${name}</div>
+    <div class="card-body">
+      <h5 class="card-title">ID: ${id}</h5>
+      <p class="card-text">ABV: ${abv}%</p>
+    </div>
   </div>
 `
 
-const errorComponent = (error) => `
+const errorComponent = ({error, message}) => `
+  <div class="card text-bg-danger mb-3" style="max-width: 18rem;">
+    <div class="card-header">${error}</div>
+    <div class="card-body">
+      <p class="card-text">${message}</p>
+    </div>
+  </div>
+`
+
+const inputComponent = () => `
   <div>
-    <h2>${error.error}</h2>
-    <h3>${error.message}</h3>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="enter beer ID">
+    <button type="button" class="btn btn-outline-warning">fetch!</button>
   </div>
 `
 
 const makeDomFromData = (element, data) => {
-  element.innerHTML = ""
-  if (data.id) element.insertAdjacentHTML("beforeend", beerComponent(data))
-  else if (data.length) data.forEach(beer => element.insertAdjacentHTML("beforeend", beerComponent(beer))) 
-  else element.insertAdjacentHTML("beforeend", errorComponent(data))
+  element.innerHTML = skeleton()
+  const headerElement = document.querySelector("header")
+  const mainElement = document.querySelector("main")
+
+  headerElement.insertAdjacentHTML("beforeend", inputComponent())
+
+  const buttonElement = document.querySelector("button")
+  buttonElement.addEventListener("click", async () =>{
+    const beerId = document.querySelector("input").value
+    
+    const newBeerData = await fetchUrl(`/beers/${beerId}`)
+    
+    makeDomFromData(rootElement, newBeerData)
+  })
+
+  if (data.id) mainElement.insertAdjacentHTML("beforeend", beerComponent(data))
+  else if (data.length) data.forEach(beer => mainElement.insertAdjacentHTML("beforeend", beerComponent(beer))) 
+  else mainElement.insertAdjacentHTML("beforeend", errorComponent(data))
 }
 
 async function init() {
   const beerData = await fetchUrl("/beers")
-  console.log(beerData)
 
   makeDomFromData(rootElement, beerData)
 }
